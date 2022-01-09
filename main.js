96bd41155f09649093719c99426f77672da523ea
soumyasrishti01 <78192188+soumyasrishti01@users.noreply.github.com>
2022-01-09 14:01:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
-function StartGame(){
+rightWrist_x = "";
+rightWrist_y = "";
+rightWrist_score = "";
+
+function preload() {
+    canvas = createCanvas(800, 800);
+    canvas.parent('canvas');
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(800, 800);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+
+        rightWrist_score = results;
+    }
+}
+
+function StartGame() {
 
 }
 
-function RestartGame(){
-    
+function RestartGame() {
+
+}
+
+function draw() {
+    if(rightWrist_score > 0.2){
+        fill("red");
+        stroke("red");
+        circle(rightWrist_x, rightWrist_y, 20);
+    }
+    image(video, 0, 0, 800, 800);
 }
